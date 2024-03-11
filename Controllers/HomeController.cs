ac3a03c9a00f83c874a699767a938457b6beac23
Gisell Bouvier <gbouvier523@gmail.com>
2024-03-11 12:17:12 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using LoanCalculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LoanCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            
+            ViewBag.FV = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(LoanCalculatorModel model)
+        {
+            
+            ViewBag.FV = model.CalculateLoanAmount();
+            return View(model);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/LoanCalculatorModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace LoanCalculator.Models
+{
+    public class LoanCalculatorModel
+    {
+        [Required(ErrorMessage = "Please enter a name.")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Please enter yearly salary.")]
+        public double? YearlySalary { get; set; }
+        [Required(ErrorMessage = "Please enter credit code.")]
+        [Range(1, 3, ErrorMessage = "Credit code must be between 1 and 3.")]
+        public int? CreditCode { get; set; }
+        public double? CalculateLoanAmount()
+        {
+            double? loanAmount = YearlySalary * .10;
+            if (CreditCode > 0)
+            {
+                if (CreditCode == 1)
+                {
+                    loanAmount += YearlySalary * .05;
+                }
+                else
+                {
+                    loanAmount += YearlySalary * .02;
+                }
+            }
+            else
+            {
+                loanAmount = 0;
+            }
+            return loanAmount;
+        }
+    }
+}
